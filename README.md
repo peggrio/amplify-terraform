19d65478de1e01da92abdf0c96bd61fbb6a9931b
Peizhen Liao <peizhenliao@Peizhens-MacBook-Pro.local>
2025-02-10 17:45:29 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# amplify-terraform
+# Automating Deployment of AWS Amplify Apps with Terraform 🎉
+
+This workshop is a guided example of how you can leverage Terraform and the **[AWS Amplify Libraries for JavaScript](https://docs.amplify.aws/lib/q/platform/js/)** to build fullstack AWS Amplify Apps. With this process, you can have full control over your backend resources, while gaining the benefits of what Amplify can help automate for you.
+
+
+For getting started quickly, check out **[code examples](https://github.com/novekm/amplify-with-terraform/tree/main/terraform-deployment/examples)**.
+
+For detailed information and step-by-step-guidance, check out the full workshop on **[AWS Workshop Studio](https://catalog.workshops.aws/amplify-with-terraform)**.
+
+
+## Deploy
+For starting backend:
+
+`cd terraform-deployment`
+
+`terraform init`(don't forget to export your aws profile with sufficient permission to run this app)
+
+`terraform fmt`
+`terraform plan`
+`terraform apply`
---
+++ terraform-deployment/main.tf
@@ -0,0 +1,50 @@
+// This is a template file for a basic deployment.
+// Modify the parameters below with actual values
+module "sample-qs" {
+  source = "./modules/sample-module"
+
+  path_to_build_spec = "../amplify.yml"
+
+  # - Amplify App -
+  # Connect Amplify to GitHub
+  existing_repo_url              = "https://github.com/peggrio/daily_practice"
+  lookup_ssm_github_access_token = true                                         // set to true if the resource exists in your AWS Account
+  ssm_github_access_token_name   = "github-access-token-amplify-terraform-proj" // name of the parameter in SSM
+  app_name                       = "peizhen-dp"
+
+
+  cognito_groups = {
+    Admin : {
+      name        = "Admin"
+      description = "Admin users"
+    },
+    Standard : {
+      name        = "Standard"
+      description = "Standard users"
+    },
+
+  }
+
+  cognito_users = {
+    # Admin Users to create
+    NarutoUzumaki : {
+      username         = "peizhenliao"
+      given_name       = "peizhen"
+      family_name      = "liao"
+      email            = "peizhenliao23@gmail.com"
+      email_verified   = true // no touchy
+      group_membership = ["Admin", "Standard"]
+    },
+
+    # Standard Users to create
+    SasukeUchiha : {
+      username         = "lucaswu"
+      given_name       = "lucas"
+      family_name      = "wu"
+      email            = "lucaswu1999@gmail.com"
+      email_verified   = true // no touchy
+      group_membership = ["Standard"]
+    }
+  }
+
+}
